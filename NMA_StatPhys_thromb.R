9baceaf045a56abb0cb6c68e07e843cfa4fc26d3
Annabel L Davies <50861690+AnnieDavies@users.noreply.github.com>
2022-07-19 12:01:59 +0100

    Update NMA_StatPhys_thromb.R

--- NMA_StatPhys_thromb.R
+++ NMA_StatPhys_thromb.R
@@ -1,8 +1,6 @@
 library("readxl")
 library("writexl")
-library("devtools")
-
-load_all("netmeta-develop")
+library(netmeta)
 
 #read in data from Dias et al (2010)
 exceldata <- read_excel(".../Dias2010.xlsx")
@@ -17,11 +15,11 @@ pDias <- pairwise(list(treat1, treat2, treat3),
                   sm = "OR")
 netDias <- netmeta(pDias)
 
-#Create forest plot (Fig ... in Davies and Galla (2022))
+#Create forest plot (Fig 7 in Davies and Galla (2022))
 forest(netDias, backtransf = FALSE, equal.size = FALSE,
        labels = c("T_1","T_2","T_3","T_4","T_5","T_6","T_7","T_8","T_9"),
        smlab = "Comparison: other vs T_1 \n (Random Effects Model)")
 
-#Create rankogram plots (Fig .. in Davies and Galla (2022))
+#Create rankogram plots (Fig 8 in Davies and Galla (2022))
 netrank <- rankogram(netDias)
 plot(netrank, sort = FALSE)
